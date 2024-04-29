9ff8b6bfb1047fdc0fafdf32a2be50241529519f
Harris Rashid <hrashid@clippers.com>
2024-04-29 15:11:33 -0700

    fonts and colors

--- components/Index/index.vue
+++ components/Index/index.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="h-screen w-full bg-fuchsia-950 flex justify-center items-center">
-    <h1 class="text-white text-8xl">Harris Rashid</h1>
+  <div class="h-screen w-full bg-red flex justify-center items-center
+  text-white text-8xl font-dela uppercase">
+    <div>  
+      <div>
+        <span class="letter">H</span>
+        <span>arris</span>
+      </div>
+      <div>
+        <span class="letter2">R</span>
+        <span>ashid</span>
+      </div>
+    </div>
   </div>
-  <div class="h-screen w-full bg-red-950 flex justify-center items-center">
-    <h2 class="header-2  text-white text-8xl">A fresh start</h2>
-  </div>
+  
 </template>
 
 <script setup lang="ts">
 const { $gsap, $ScrollTrigger } = useNuxtApp()
 onMounted( () => {
   // Header shrinking
-  $gsap.to(".header-2", { rotation: 27, x: 100, duration: 1 });
-
+  $gsap.to(".letter", { rotation: 27, duration: 1 });
+  $gsap.to(".letter2", { rotation: -27, duration: 1 });
 })
 </script>
 
 <style scoped>
-div, h1 {
-  font-family: Roboto Serif, sans-serif;
-}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ import colors from 'tailwindcss/colors'
 export default {
   theme: {
     extend: {
+      fontFamily: {
+        dela: ['Dela Gothic One', 'sans-serif'], 
+      },
       colors: {
-        primary: colors.green
+        red: "#cf152d",
       }
     }
   }
