bc0ceeefe39916ccd677f580ef0423ca6aaa77a0
Harris Rashid <hrashid@clippers.com>
2024-05-01 12:57:35 -0700

    photo

--- components/Index/indexSection1Intro.vue
+++ components/Index/indexSection1Intro.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="mt-16 border-solid border-0 border-t border-white text-lg lg:text-2xl uppercase text-white font-jojo pt-28">  
-    <div class="mb-2 out opacity-0">Creative Technologist.</div> 
-    <div class="mb-2 out opacity-0">Digital Strategist.</div> 
-    <div class="mb-2 out opacity-0 ">
-      <div class="inline-block mr-2">Web Developer</div>
-      <div class="inline-block flip">Extraordinaire</div>.
+    <div class="lg:flex pl-12">
+      <div class="">
+        <img src="/headshot_harris_rashid.png" class="w-36 contrast-100 saturate-100 drop-shadow-lg opacity-0 profile -mt-8">
+      </div>
+      <div>
+        <div class="mb-2 fade out opacity-0">Creative Technologist.</div> 
+        <div class="mb-2 fade out opacity-0">Digital Strategist.</div> 
+        <div class="mb-2 fade out opacity-0 ">
+          <div class="inline-block mr-2">Web Developer</div>
+          <div class="inline-block flip">Extraordinaire</div>.
+        </div>
+        <div class="text-sm mt-2 fade opacity-0">
+          <a href="mailto:hello@harrisrashid.com" class="hover:underline">hello@harrisrashid.com</a>
+        </div>
+      </div>
     </div>
-    <div class="text-sm mt-8 out opacity-0">
-      <a href="mailto:hello@harrisrashid.com" class="hover:underline">hello@harrisrashid.com</a>
-    </div>
+    
   </div>
 </template>
 
@@ -17,7 +25,7 @@
 const { $gsap } = useNuxtApp()
 onMounted( () => {
   let tl = $gsap.timeline().delay(1);
-  tl.to(".out", { 
+  tl.to(".fade", { 
     y: -30, 
     opacity: 1,
     ease: "power1.out",
@@ -34,7 +42,14 @@ onMounted( () => {
     repeat: -1,
     repeatDelay:1.5,
     yoyo: true
-  });  
+  }); 
+  let tl3 = $gsap.timeline().delay(2);
+  tl3.to(".profile", { 
+    x: -50, 
+    opacity: 1,
+    ease: "power1.out",
+    duration: 1, 
+  }); 
 })
 </script>
 
