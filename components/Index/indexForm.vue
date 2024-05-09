49100238e8d47add82f195621bfba0912dc062f0
Harris Rashid <hrashid@clippers.com>
2024-05-09 14:53:49 -0700

    test deploy

---
+++ components/Index/indexForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>    
+    <div id="form"></div>   
+  </div>
+</template>
+<script setup lang="ts">
+  const { $postscribe } = useNuxtApp()
+  onMounted(() => {
+    console.log("mounted")
+    const form = () =>  $postscribe('<h1>Hello PostScribe</h1>')
+  })
+</script>
+
--- components/Index/indexSection1Intro.vue
+++ components/Index/indexSection1Intro.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-4 lg:mt-16 border-solid border-0 border-t border-white/50 font-jojo pt-16 lg:pt-28">  
-    <div class="w-full lg:w-2/3 lg:mx-auto bg-white/20 rounded-md p-8 lg:p-12 lg:pb-0 shadow-2xl">
+    <div class="pre opacity-0 w-full lg:w-2/3 lg:mx-auto bg-white/20 rounded-md p-8 lg:p-12 lg:pb-0 shadow-2xl">
       <div class="grid md:grid-cols-12 lg:gap-12">
         <div class="lg:col-span-4 px-8">
           <img src="/headshot_harris_rashid.jpg" class="bg-white rounded-md p-4 w-full contrast-100 saturate-100 opacity-0 profile">
@@ -23,6 +23,14 @@
 <script setup lang="ts">
 const { $gsap } = useNuxtApp()
 onMounted( () => {
+  let tlpre = $gsap.timeline().delay(.5);
+  tlpre.to(".pre", { 
+    y: -30, 
+    opacity: 1,
+    ease: "power1.out",
+    duration: 1
+  }); 
+  
   let tl = $gsap.timeline().delay(1.5);
   tl.to(".fade", { 
     y: -30, 
